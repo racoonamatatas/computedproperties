e4ed62913672a9f3d25be405f0cbb740d4b8cb97
racoonamatatas <emacanghaill@gmail.com>
2026-01-07 15:56:44 +0100

    feat: Display people and computed children arrays using v-for

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
     <input v-model.trim="newName" type="text" placeholder="Name" />
     <input v-model.number="newAge" placeholder="Age" />
     <button @click="addPerson">Add</button>
+
+    <ul>
+        People:
+        <br />
+        <li v-for="person in people" :key="person.name">{{ person.name }} {{ person.age }}</li>
+    </ul>
+    <ul>
+        Children:
+        <li v-for="child in children" :key="child.name">{{ child.name }} {{ child.age }}</li>
+    </ul>
 </template>
 
 <script setup>
