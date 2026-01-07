e2384e62daa12199fa58918c43337b2d36da6c74
racoonamatatas <emacanghaill@gmail.com>
2026-01-07 14:21:22 +0100

    feat: Add reactive refs and inputs for 'name' and 'age'

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
-<template></template>
+<template>
+    <input v-model.trim="newName" type="text" placeholder="Name" />
+    <input v-model.number="newAget" placeholder="Age" />
+</template>
 
 <script setup>
 import {ref} from 'vue';
@@ -7,4 +10,7 @@ const people = ref([
     {name: 'Jan', age: 12},
     {name: 'Piet', age: 20},
 ]);
+
+let newName = ref('');
+let newAge = ref();
 </script>
