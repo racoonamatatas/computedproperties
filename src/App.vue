b6b4b98c2887c30ed68c9f73a65cb0cc4c139c9f
racoonamatatas <emacanghaill@gmail.com>
2026-01-07 15:29:13 +0100

    feat: Add button and function to push person into people array

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <input v-model.trim="newName" type="text" placeholder="Name" />
-    <input v-model.number="newAget" placeholder="Age" />
+    <input v-model.number="newAge" placeholder="Age" />
+    <button @click="addPerson">Add</button>
 </template>
 
 <script setup>
@@ -13,4 +14,13 @@ const people = ref([
 
 let newName = ref('');
 let newAge = ref();
+
+// Adds a new person to the reactive people array.
+const addPerson = () => {
+    people.value.push({name: newName.value, age: newAge.value});
+
+    // reset the ref values
+    newName.value = '';
+    newAge.value = null;
+};
 </script>
