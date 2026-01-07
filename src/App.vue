791a076c7b34cf782d66dcb59e17d89b5cb196cb
racoonamatatas <emacanghaill@gmail.com>
2026-01-07 14:03:27 +0100

    Add reactive 'people' array

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
-<script setup></script>
-
 <template></template>
+
+<script setup>
+import {ref} from 'vue';
+
+const people = ref([
+    {name: 'Jan', age: 12},
+    {name: 'Piet', age: 20},
+]);
+</script>
